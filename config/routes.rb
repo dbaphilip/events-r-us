bf4f6df96da8610468bfba496a04d9a10c9628a4
pagaba <pagaba>
2026-06-28 10:59:13 +0100

    build edit form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get "events" => "events#index"
+  get "events/:id/edit" => "events#edit", as: :edit_event
   get "events/:id" => "events#show", as: :event
   get "up" => "rails/health#show", as: :rails_health_check
   root "events#index"
