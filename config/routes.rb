66574cfb329013cabf281a1b5baca8028910cbe1
pagaba <pagaba>
2026-06-30 09:03:46 +0100

    finish forms/edit

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get "events" => "events#index"
   get "events/:id/edit" => "events#edit", as: :edit_event
   get "events/:id" => "events#show", as: :event
+  patch "events/:id" => "events#update"
   get "up" => "rails/health#show", as: :rails_health_check
   root "events#index"
 end
