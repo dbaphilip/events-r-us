fc9b328d4b21d74b2b38fe41df51cb71ed385d45
dbaphilip <56190044+dbaphilip@users.noreply.github.com>
2026-06-30 09:43:33 +0100

    finish forms/new (#7)
    Co-authored-by: pagaba <pagaba>

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,15 @@ class EventsController < ApplicationController
     @event = Event.find(params[:id])
   end
 
+  def new
+    @event = Event.new
+  end
+
+  def create
+    e = Event.create(event_params)
+    redirect_to e
+  end
+
   def edit
     @event = Event.find(params[:id])
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get "events" => "events#index"
-  get "events/:id/edit" => "events#edit", as: :edit_event
-  get "events/:id" => "events#show", as: :event
-  patch "events/:id" => "events#update"
+  resources :events
   get "up" => "rails/health#show", as: :rails_health_check
   root "events#index"
 end
